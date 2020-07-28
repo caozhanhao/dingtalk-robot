4228cf88164421cc45b18e9ee520c1885cb7fa1f
caozhanhao <cao2013zh@163.com>
2020-07-28 17:01:44 +0800

    修改robot.py

--- robot.py
+++ robot.py
@@ -5,39 +5,29 @@ import base64
 import urllib.parse
 import requests
 import json
-
-webhook = '输入你的webhook地址'
-
-
+webhook = 'webhook地址'
 timestamp = str(round(time.time() * 1000))
-secret = '输入你的密钥'
+secret = '密钥'
 secret_enc = secret.encode('utf-8')
 string_to_sign = '{}\n{}'.format(timestamp, secret)
 string_to_sign_enc = string_to_sign.encode('utf-8')
 hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
 sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
-def getDingMes():
+baseUrl = webhook+'&timestamp='+timestamp+"&sign="+sign
 
-    baseUrl = webhook+'&timestamp='+timestamp+"&sign="+sign
+HEADERS = {
+    "Content-Type": "application/json ;charset=utf-8 "
+}
+message=input("你要发送的消息：")
+stringBody ={
+    "msgtype": "text",
+    "text": {"content": message},
+    "at": {
+        "atMobiles": ["1825718XXXX"],
+            "isAtAll": False
 
-    HEADERS = {
-        "Content-Type": "application/json ;charset=utf-8 "
-    }
-    message=input("你要发送的消息：")
-    stringBody ={
-        "msgtype": "text",
-        "text": {"content": message},
-        "at": {
-            "atMobiles": ["1825718XXXX"],
-               "isAtAll": False
-
-        }
- }
-    MessageBody = json.dumps(stringBody)
-    result = requests.post(url=baseUrl, data=MessageBody, headers=HEADERS)
-    print(result.text)
-
-if __name__ == '__main__':
-    getDingMes()
-
-
+     }
+}
+MessageBody = json.dumps(stringBody)
+result = requests.post(url=baseUrl, data=MessageBody, headers=HEADERS)
+print(result.text)
